ce90f07813319908e355ffef6f16bc52c24a284c
BaTung <tunghoang5161997@gmail.com>
2025-09-24 08:14:14 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-    <div>Huynh be de</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
